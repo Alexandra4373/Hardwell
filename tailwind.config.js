df6a340d84ab2d176e441ae1a8c49af92f99f60b
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-23 19:24:47 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			colors: {
-				primary: "#EA3D3C",
-				second: "#39364E",
-				secondary: "#9CD1C1",
-				tert: "#F5AD0D",
-				quart: "#FB690C",
-			},
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EA3D3C",
+        second: "#39364E",
+        secondary: "#9CD1C1",
+        tert: "#F5AD0D",
+        quart: "#FB690C",
+      },
 
-			fontFamily: {
-				open: ["Open Sans", "sans-serif"],
-				epilogue: ["Epilogue", "sans-serif"],
-			},
+      fontFamily: {
+        open: ["Open Sans", "sans-serif"],
+        epilogue: ["Epilogue", "sans-serif"],
+      },
 
-			container: {
-				padding: "1.2rem",
-				center: true,
-			},
-		},
-	},
-	plugins: [],
+      container: {
+        padding: "1.2rem",
+        center: true,
+      },
+    },
+  },
+  plugins: [],
 };
